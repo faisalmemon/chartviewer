72a42890a6b96bcad7bf50f9b35c5870b2619eeb
Faisal Memon <faisalmemonuk@yahoo.com>
2012-08-11 17:36:11 +0100

    Add initial graph selection view controller
    An initial graph selection view controller which has a segmented
    control at the top for graph/pie chart/bar chart and a table view below
    to list out the different items for the given type of chart.

---
+++ chartviewer/chartviewer/cvGraphSelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  cvGraphSelectionViewController.h
+//  chartviewer
+//
+//  Created by Faisal Memon on 11/08/2012.
+//  Copyright (c) 2012 Faisal Memon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cvGraphSelectionViewController : UIViewController
+
+@end
---
+++ chartviewer/chartviewer/cvGraphSelectionViewController.m
@@ -0,0 +1,44 @@
+//
+//  cvGraphSelectionViewController.m
+//  chartviewer
+//
+//  Created by Faisal Memon on 11/08/2012.
+//  Copyright (c) 2012 Faisal Memon. All rights reserved.
+//
+
+#import "cvGraphSelectionViewController.h"
+
+@interface cvGraphSelectionViewController ()
+
+@end
+
+@implementation cvGraphSelectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
